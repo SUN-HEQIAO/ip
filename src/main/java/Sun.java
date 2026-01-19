8b90ae8860dffadad5614fc4ca43c65fd0ca372e
SUN_HEQIAO <e1355839@u.nus.edu>
2026-01-19 23:30:52 +0800

    Implemented requirements in Level 1.

--- src/main/java/Sun.java
+++ src/main/java/Sun.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
 public class Sun {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Sun.");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
 
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println(input);
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
