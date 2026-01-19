a794e1667e092bf3a6fc37100b9493cabffcab25
SUN_HEQIAO <e1355839@u.nus.edu>
2026-01-19 23:44:41 +0800

    Implemented requirements in Level 2.

--- src/main/java/Sun.java
+++ src/main/java/Sun.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Sun {
@@ -6,12 +7,23 @@ public class Sun {
         System.out.println("What can I do for you?");
 
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
+
         while (true) {
             String input = scanner.nextLine();
+
             if (input.equalsIgnoreCase("bye")) {
                 break;
             }
-            System.out.println(input);
+
+            if (input.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(String.format("%d. %s", i+1, tasks.get(i)));
+                }
+            } else {
+                tasks.add(input);
+                System.out.println(String.format("added: %s", input));
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
