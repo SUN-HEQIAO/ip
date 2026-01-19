05ced117dab8bcf0e3ca920a35097791eba0c8bc
SUN_HEQIAO <e1355839@u.nus.edu>
2026-01-19 23:05:57 +0800

    Implemented initial skeletal version that simply greets the user and exits.

--- src/main/java/Sun.java
+++ src/main/java/Sun.java
@@ -1,10 +1,11 @@
 public class Sun {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Sun.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
     }
 }
