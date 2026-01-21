28a4d6444a729abc3dd6391093d2e11e4ef32848
SUN_HEQIAO <e1355839@u.nus.edu>
2026-01-21 09:49:41 +0800

    Implemented requirements in Level 3. - I added a new "Task" class. - I refactored the "Sun" class. - Now using switch statements and a helper function to iterate through the list of Task objects.

--- src/main/java/Sun.java
+++ src/main/java/Sun.java
@@ -7,24 +7,47 @@ public class Sun {
         System.out.println("What can I do for you?");
 
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         while (true) {
             String input = scanner.nextLine();
+            String[] inputs = input.split(" ");
+            String command = inputs[0];
 
-            if (input.equalsIgnoreCase("bye")) {
+            if (command.equalsIgnoreCase("bye")) {
                 break;
             }
 
-            if (input.equals("list")) {
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println(String.format("%d. %s", i+1, tasks.get(i)));
-                }
-            } else {
-                tasks.add(input);
-                System.out.println(String.format("added: %s", input));
+            switch (command) {
+                case "list":
+                    goThroughList((tasks));;
+                    break;
+                case "mark":
+                    if (inputs.length == 2) {
+                        int index = Integer.parseInt(inputs[1]) - 1;
+                        tasks.get(index).setIsDone(true);
+                        goThroughList(tasks);
+                    }
+                    break;
+                case "unmark":
+                    if (inputs.length == 2) {
+                        int index = Integer.parseInt(inputs[1]) - 1;
+                        tasks.get(index).setIsDone(false);
+                        goThroughList(tasks);
+                    }
+                    break;
+                default:
+                    tasks.add(new Task(input));
+                    System.out.println("added: " + input);
+
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static void goThroughList(ArrayList<Task> tasks) {
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(String.format("%d. %s", i + 1, tasks.get(i)));
+        }
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,28 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getIsDone() {
+        return this.isDone ? "X" : " ";
+    }
+
+    public void setIsDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", this.getIsDone(), this.getDescription());
+    }
+
+
+}
