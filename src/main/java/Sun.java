a5536ae43d7b412398566da2025e78bdcab0e02a
SUN_HEQIAO <e1355839@u.nus.edu>
2026-01-21 12:14:23 +0800

    Refactored Sun.java with helper methods

--- src/main/java/Sun.java
+++ src/main/java/Sun.java
@@ -1,20 +1,24 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import static java.util.Arrays.copyOfRange;
-
 public class Sun {
     public static void main(String[] args) {
+        // Opening Message
         System.out.println("Hello! I'm Sun.");
         System.out.println("What can I do for you?");
 
+        // Prepare Scanner and ArrayList<Task>
         Scanner scanner = new Scanner(System.in);
         ArrayList<Task> tasks = new ArrayList<>();
 
+        // Input loop using Switch Statements
         while (true) {
+            // Parse input into 2 parts first.
+            // Goal is to get command first
             String input = scanner.nextLine();
-            String[] inputs = input.split(" ");
+            String[] inputs = input.split(" ", 2);
             String command = inputs[0];
+            String rest = (inputs.length > 1) ? inputs[1] : "";
 
             if (command.equalsIgnoreCase("bye")) {
                 break;
@@ -23,51 +27,31 @@ public class Sun {
             switch (command) {
                 case "list":
                     System.out.println("Here are the tasks in your list:");
-                    goThroughList((tasks));
+                    goThroughList(tasks);
                     break;
 
                 case "mark":
-                    if (inputs.length == 2) {
-                        int index = Integer.parseInt(inputs[1]) - 1;
-                        Task targetTask = tasks.get(index);
-                        targetTask.setIsDone(true);
-                        System.out.println("Nice! I've marked this task as done:");
-                        System.out.println(targetTask);
-                    }
+                    handleMark(tasks, rest, true);
                     break;
 
                 case "unmark":
-                    if (inputs.length == 2) {
-                        int index = Integer.parseInt(inputs[1]) - 1;
-                        Task targetTask = tasks.get(index);
-                        targetTask.setIsDone(false);
-                        System.out.println("OK, I've marked this task as not done yet:");
-                        System.out.println(targetTask);
-                    }
+                    handleMark(tasks, rest, false);
                     break;
 
                 case "todo":
-                    String description = String.join(" ", copyOfRange(inputs, 1, inputs.length));
-                    Task todoTask = new Todo(description);
-                    tasks.add(todoTask);
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(todoTask);
-                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                    handleTodo(tasks, rest);
                     break;
 
                 case "deadline":
-                    String description = String.join(" ", copyOfRange(inputs, 1, inputs.length));
-                    Task todoTask = new Todo(description);
-                    tasks.add(todoTask);
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(todoTask);
-                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                    handleDeadline(tasks, rest);
+                    break;
+
+                case "event":
+                    handleEvent(tasks, rest);
                     break;
 
                 default:
-                    tasks.add(new Task(input));
-                    System.out.println("added: " + input);
-
+                    System.out.println("Please state your command");
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
@@ -77,5 +61,48 @@ public class Sun {
         for (int i = 0; i < tasks.size(); i++) {
             System.out.println(String.format("%d. %s", i + 1, tasks.get(i)));
         }
+    }
+
+    private static void handleMark(ArrayList<Task> tasks, String rest, boolean isDone) {
+        int index = Integer.parseInt(rest) - 1;
+        Task targetTask = tasks.get(index);
+        targetTask.setIsDone(isDone);
+        System.out.println(isDone ? "Nice! I've marked this task as done:"
+                                  : "OK, I've marked this task as not done yet:");
+        System.out.println(targetTask);
+    }
+
+    private static void printTaskAdded(Task task, int taskCount) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println(task);
+        System.out.println(String.format("Now you have %d tasks in the list.", taskCount));
+    }
+
+    private static void handleTodo(ArrayList<Task> tasks, String rest) {
+        Task todoTask = new Todo(rest);
+        tasks.add(todoTask);
+        printTaskAdded(todoTask, tasks.size());
+    }
+
+    private static void handleDeadline(ArrayList<Task> tasks, String rest) {
+        String[] parts = rest.split(" /by ", 2);
+        String description = parts[0];
+        String by = parts[1];
+        Task deadlineTask = new Deadline(description, by);
+        tasks.add(deadlineTask);
+        printTaskAdded(deadlineTask, tasks.size());
+    }
+
+    private static void handleEvent(ArrayList<Task> tasks, String rest) {
+        String[] descriptionSplit = rest.split(" /from ", 2);
+        String description = descriptionSplit[0];
+
+        String[] fromSplit = descriptionSplit[1].split(" /to ", 2);
+        String from = fromSplit[0];
+        String to = fromSplit[1];
+
+        Task eventTask = new Event(description, from, to);
+        tasks.add(eventTask);
+        printTaskAdded(eventTask, tasks.size());
     }
 }
