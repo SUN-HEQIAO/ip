fc6f80c9d6848c69ba614c0c5a5e4265d4b243f4
SUN_HEQIAO <e1355839@u.nus.edu>
2026-01-21 13:39:52 +0800

    - Cleaned up user inputs - Added custom exceptions for error handling. - Refactored Sun.java class to be more modular.

---
+++ src/main/java/Exceptions.java
@@ -0,0 +1,19 @@
+class InvalidCommandException extends Exception {
+    public InvalidCommandException(String message) { super(message); }
+}
+
+class InvalidTaskNumberException extends Exception {
+    public InvalidTaskNumberException(String message) { super(message); }
+}
+
+class InvalidTodoException extends Exception {
+    public InvalidTodoException(String message) { super(message); }
+}
+
+class InvalidDeadlineException extends Exception {
+    public InvalidDeadlineException(String message) { super(message); }
+}
+
+class InvalidEventException extends Exception {
+    public InvalidEventException(String message) { super(message); }
+}
--- src/main/java/Sun.java
+++ src/main/java/Sun.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Sun {
+    // Main
     public static void main(String[] args) {
         // Opening Message
         System.out.println("Hello! I'm Sun.");
@@ -13,58 +14,101 @@ public class Sun {
 
         // Input loop using Switch Statements
         while (true) {
-            // Parse input into 2 parts first.
-            // Goal is to get command first
-            String input = scanner.nextLine();
-            String[] inputs = input.split(" ", 2);
-            String command = inputs[0];
-            String rest = (inputs.length > 1) ? inputs[1] : "";
+            // Trim user input
+            String input = scanner.nextLine().trim();
 
-            if (command.equalsIgnoreCase("bye")) {
+            // Skip empty inputs ("")
+            if (input.isEmpty()) continue;
+
+            // Normalise spaces
+            input = input.replaceAll("\\s+", " ");
+
+            // If user inputs "bye"
+            if (input.equalsIgnoreCase("bye")) {
                 break;
             }
 
-            switch (command) {
-                case "list":
-                    System.out.println("Here are the tasks in your list:");
-                    goThroughList(tasks);
-                    break;
-
-                case "mark":
-                    handleMark(tasks, rest, true);
-                    break;
-
-                case "unmark":
-                    handleMark(tasks, rest, false);
-                    break;
-
-                case "todo":
-                    handleTodo(tasks, rest);
-                    break;
-
-                case "deadline":
-                    handleDeadline(tasks, rest);
-                    break;
-
-                case "event":
-                    handleEvent(tasks, rest);
-                    break;
-
-                default:
-                    System.out.println("Please state your command");
+            // Process inputs
+            try {
+                processInputs(input, tasks);
+            } catch (InvalidCommandException | InvalidTaskNumberException |
+                     InvalidTodoException | InvalidDeadlineException |
+                     InvalidEventException e) {
+                System.out.println(e.getMessage());
             }
+
         }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 
+    //Starter Motor
+    private static void processInputs(String input, ArrayList<Task> tasks)
+            throws InvalidCommandException, InvalidTodoException,
+            InvalidDeadlineException, InvalidEventException, InvalidTaskNumberException {
+
+        String[] inputs = input.split(" ", 2);
+        String command = inputs[0].toLowerCase(); //only for command
+        String rest = (inputs.length > 1) ? inputs[1] : "";
+
+        switch (command) {
+            case "list":
+                System.out.println("Here are the tasks in your list:");
+                goThroughList(tasks);
+                break;
+
+            case "mark":
+                handleMark(tasks, rest, true);
+                break;
+
+            case "unmark":
+                handleMark(tasks, rest, false);
+                break;
+
+            case "todo":
+                handleTodo(tasks, rest);
+                break;
+
+            case "deadline":
+                handleDeadline(tasks, rest);
+                break;
+
+            case "event":
+                handleEvent(tasks, rest);
+                break;
+
+            default:
+                throw new InvalidCommandException("OOPS!!! I'm sorry, but I don't know what that command means :-(");
+        }
+    }
+
+    // Helper Methods
     private static void goThroughList(ArrayList<Task> tasks) {
         for (int i = 0; i < tasks.size(); i++) {
             System.out.println(String.format("%d. %s", i + 1, tasks.get(i)));
         }
     }
 
-    private static void handleMark(ArrayList<Task> tasks, String rest, boolean isDone) {
-        int index = Integer.parseInt(rest) - 1;
+    private static void handleMark(ArrayList<Task> tasks, String rest, boolean isDone)
+            throws InvalidTaskNumberException {
+        // No task number specified
+        if (rest.isEmpty()) {
+            throw new InvalidTaskNumberException(" OOPS!!! Task number missing.");
+        }
+
+        // Task number is not numerical
+        int index;
+        try {
+            index = Integer.parseInt(rest) - 1;
+        } catch (NumberFormatException e) {
+            throw new InvalidTaskNumberException("OOPS!!! Please enter a valid numerical task number.");
+        }
+
+        // Task number is out of range
+        if (index < 0 || index >= tasks.size()) {
+            throw new InvalidTaskNumberException("OOPS!!! Task number out of range.");
+        }
+
         Task targetTask = tasks.get(index);
         targetTask.setIsDone(isDone);
         System.out.println(isDone ? "Nice! I've marked this task as done:"
@@ -78,14 +122,25 @@ public class Sun {
         System.out.println(String.format("Now you have %d tasks in the list.", taskCount));
     }
 
-    private static void handleTodo(ArrayList<Task> tasks, String rest) {
+    private static void handleTodo(ArrayList<Task> tasks, String rest)
+            throws InvalidTodoException {
+        // todoTask with no description
+        if (rest.isEmpty()) throw new InvalidTodoException("OOPS!!! The description of a todo cannot be empty.");
+
         Task todoTask = new Todo(rest);
         tasks.add(todoTask);
         printTaskAdded(todoTask, tasks.size());
     }
 
-    private static void handleDeadline(ArrayList<Task> tasks, String rest) {
+    private static void handleDeadline(ArrayList<Task> tasks, String rest)
+            throws InvalidDeadlineException {
         String[] parts = rest.split(" /by ", 2);
+
+        // Deadline with no description or due-date
+        if (parts.length < 2 || parts[0].isEmpty() || parts[1].isEmpty()) {
+            throw new InvalidDeadlineException("OOPS!!! The description or due date of a deadline cannot be empty.");
+        }
+
         String description = parts[0];
         String by = parts[1];
         Task deadlineTask = new Deadline(description, by);
@@ -93,11 +148,22 @@ public class Sun {
         printTaskAdded(deadlineTask, tasks.size());
     }
 
-    private static void handleEvent(ArrayList<Task> tasks, String rest) {
+    private static void handleEvent(ArrayList<Task> tasks, String rest)
+            throws InvalidEventException {
         String[] descriptionSplit = rest.split(" /from ", 2);
+
+        // Event with no description or start
+        if (descriptionSplit.length < 2 || descriptionSplit[0].isEmpty() || descriptionSplit[1].isEmpty()) {
+            throw new InvalidEventException("OOPS!!! The description or start time of an event cannot be empty.");
+        }
         String description = descriptionSplit[0];
 
         String[] fromSplit = descriptionSplit[1].split(" /to ", 2);
+
+        // Event with no start or end
+        if (fromSplit.length < 2 || fromSplit[0].isEmpty() || fromSplit[1].isEmpty())
+            throw new InvalidEventException("OOPS!!! The start or end time of an event cannot be empty.");
+
         String from = fromSplit[0];
         String to = fromSplit[1];
 
