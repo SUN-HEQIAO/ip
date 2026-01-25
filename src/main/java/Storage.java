777d5b35c0e5fbaa06136042a74d42c0a29e640b
SUN_HEQIAO <e1355839@u.nus.edu>
2026-01-25 16:38:50 +0800

    Create Storage class

---
+++ src/main/java/Storage.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Storage {
+    private final String filePath;
+
+    public Storage(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public ArrayList<Task> load() throws IOException {
+        ArrayList<Task> tasks = new ArrayList<>();
+        File file = new File(filePath);
+
+        if (!file.exists()) {
+            return tasks;
+        }
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                tasks.add(Task.fromFileString(scanner.nextLine());
+            }
+        }
+
+        return tasks;
+    }
+
+    public void save(TaskList tasks) throws IOException {
+        File file = new File(filePath);
+        File parent = file.getParentFile();
+
+        if (parent != null && !parent.exists()) {
+            parent.mkdirs();
+        }
+
+        try (FileWriter writer = new FileWriter(filePath)) {
+            for (Task task : tasks.getAll()) {
+                writer.write(task.toFileString());
+                writer.write(System.lineSeparator());
+            }
+        }
+    }
+}
