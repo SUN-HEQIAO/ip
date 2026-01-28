3c277bb50fd3873c13fa36bcd26b9da72a354e88
SUN_HEQIAO <e1355839@u.nus.edu>
2026-01-28 09:15:54 +0800

    Create Ui.java

---
+++ src/main/java/Ui.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Ui {
+    private Scanner scanner;
+
+    public Ui() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void showWelcome() {
+        System.out.println("Hello! I'm Sun.");
+        System.out.println("What can I do for you?");
+    }
+
+    public String readLine() {
+        return scanner.nextLine();
+    }
+
+    public void showLine(String message) {
+        System.out.println(message);
+    }
+
+    public void showError(String message) {
+        System.out.println(message);
+    }
+
+    public void showBye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
