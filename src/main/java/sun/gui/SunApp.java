7d20b6ed855cae638d16e225ec8203e0587d6a3d
SUN_HEQIAO <e1355839@u.nus.edu>
2026-02-02 17:24:37 +0800

    ChatBot can echo messages

--- src/main/java/sun/gui/SunApp.java
+++ src/main/java/sun/gui/SunApp.java
@@ -2,24 +2,88 @@ package sun.gui;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class SunApp extends Application {
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+
     @Override
     public void start(Stage stage) {
-        Label welcome = new Label("Welcome to Sun Chatbot!");
+        // Initialise Components
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
 
-        StackPane root = new StackPane();
-        root.getChildren().add(welcome);
+        userInput = new TextField();
+        sendButton = new Button("Send");
 
-        Scene scene = new Scene(root, 400, 600);
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        Scene scene = new Scene(mainLayout, 400, 600);
         stage.setScene(scene);;
-        stage.setTitle("Sun ChatBot");
         stage.show();
+
+
+        // Window settings
+        stage.setTitle("Sun ChatBot");
+        stage.setResizable(false);
+        stage.setMinHeight(600);
+        stage.setMinWidth(400);
+
+        mainLayout.setPrefSize(400, 600);
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE); // Make the VBox’s preferred height automatically fit its children, so it grows as we add messages.
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+
+        userInput.setPrefWidth(325);;
+        sendButton.setPrefWidth(55);;
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+
+        // Define Interactions
+        sendButton.setOnMouseClicked((event) -> handleUserInput());
+        userInput.setOnAction((event) -> handleUserInput());
+
     }
 
+    private void handleUserInput() {
+        String input = userInput.getText();
+        if (input.isEmpty()) {
+            return;
+        }
+
+        // Add user message
+        dialogContainer.getChildren().add(new Label("You: " + input));
+
+        // Add simple bot echo
+        dialogContainer.getChildren().add(new Label("Sun: " + input));
+
+        userInput.clear();
+
+    }
     public static void main(String[] args) {
         launch(args);
     }
